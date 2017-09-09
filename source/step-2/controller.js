cb1b636cfcc5a7794b89384e7c58f9ad3d6e2cc8
yarik-vv <yarik.vashchuk@gmail.com>
2017-09-09 22:53:49 +0300

    validation for step two

--- source/step-2/controller.js
+++ source/step-2/controller.js
@@ -1,14 +1,45 @@
 class StepTwoController {
   constructor($scope) {
+    $scope.prevPage = '/step-1';
+    $scope.nextPage = '_';
+    $scope.page2 = false;
+    $scope.currentPage = 2;
     $scope.title = 'Второй шаг анкеты';
     $scope.stepName = '2. Выберите страну и город';
-    $scope.nextPage = '/step-3';
-    $scope.prevPage = '/step-1';
 
-    this.save = () => {
-      $scope.country = 'ukraine';
-      $scope.sity = 'kiev';
-    }
+    let countryValid = false;
+    let cityValid = false;
+
+    $scope.$watch('user.$valid', () => {
+      let form = $scope.$root.user;
+
+      if (form.$valid ){
+        $scope.nextPage = '/step-3';
+        $scope.page2 = true;
+      } else {
+        $scope.nextPage = '';
+        $scope.page2 = false;
+      }
+    }, true);
+    
+    // $scope.$watch('user.country', (text) => {
+    //   countryValid = true;
+    //   if(cityValid){
+    //     $scope.nextPage = '/step-3';
+    //   } else {
+    //     $scope.nextPage = false;
+    //   }
+    // }, true);
+
+    // $scope.$watch('user.city', (text) => {
+    //   cityValid = true;
+    //   if(countryValid){
+    //     $scope.nextPage = '/step-3';
+    //   } else {
+    //     $scope.nextPage = '';
+    //   }
+    // }, true);
+
   }
 }
 
