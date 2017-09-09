8e0ad0cccfa5f6cdcd77150cde6bae70d787851d
yarik-vv <yarik.vashchuk@gmail.com>
2017-09-09 22:50:02 +0300

    validation for one step and configure controller

--- source/step-1/controller.js
+++ source/step-1/controller.js
@@ -1,26 +1,61 @@
-const currentPage = document.getElementById('step1');
+const inputs = document.getElementsByTagName('input');
+const errors = document.getElementsByTagName('p');
 
+import { validate, reset } from '../validation';
 
 class stepOneController {
   constructor($scope) {
+    $scope.prevPage = '';
+    $scope.nextPage = '_';
+    $scope.page1 = false;
+    $scope.currentPage = 1;
     $scope.title = 'Первый шаг анкеты';
-    $scope.nextPage = '/step-2';
-    $scope.prevPage = ' ';
     $scope.stepName = '1. Введите имя и email';
-    currentPage.className = 'active';
 
-    
+    let emailValid = false;
+    let nameValid = false;
 
-    if($scope.name !== undefined && $scope.email !== undefined){
-      this.name = $scope.name;
-      this.email = $scope.email;
-    }
+    $scope.$watch('user.name', (text) => {
+      const nameInput = inputs[0];
+      const nameError = errors[0];
 
-    this.save = () => {
-      console.log($scope.user.$invalid);
-      $scope.name = this.name;
-      $scope.email = this.email; 
-    }
+      if(nameInput.value.length !== 0){ 
+        if(validate('^[а-яА-ЯёЁa-zA-Z]{2,20}$', text, nameInput, nameError)){
+          nameValid = true;
+          if(emailValid){
+            $scope.nextPage = '/step-2';
+            $scope.page1 = true;
+          };
+        } else {
+          nameValid = false; 
+          $scope.nextPage = '_';  
+          $scope.page1 = false;   
+        }
+      } else {
+        reset(nameInput, nameError); 
+      }
+    }, true);
+
+    $scope.$watch('user.email', (text) => {
+      const emailInput = inputs[1];
+      const emailError = errors[1];
+
+      if(emailInput.value.length !== 0){ 
+        if(validate('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$', text, emailInput, emailError)) {
+          emailValid = true;
+          if(nameValid){
+            $scope.nextPage = '/step-2';
+            $scope.page1 = true;
+          }
+        } else {
+          emailValid = false;
+          $scope.nextPage = '_';
+          $scope.page1 = false;
+        }
+      } else {
+        reset(emailInput, emailError); 
+      }
+    }, true);
   }
 }
 
--- source/step-1/index.js
+++ source/step-1/index.js
@@ -3,7 +3,6 @@ import uirouter from 'angular-ui-router';
 
 import routing from './routes';
 import stepOneController from './controller';
-//import val from '../directive.js';
 
 export default angular.module('app.stepOne', [uirouter])
   .config(routing)
