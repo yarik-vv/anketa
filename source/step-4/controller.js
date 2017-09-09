70278c16768c2e3737727c6514f4ff2184cc6840
yarik-vv <yarik.vashchuk@gmail.com>
2017-09-09 23:00:24 +0300

    implementation select image in step four, and others

--- source/step-4/controller.js
+++ source/step-4/controller.js
@@ -2,12 +2,36 @@ class FourStepController {
   constructor($scope) {
     $scope.title = 'Четвертый шаг анкеты';
     $scope.stepName = '4. Выберите любимого котика';
-    $scope.nextPage = '/result';
+    $scope.nextPage = '';
+    $scope.page4 = false;
+    $scope.currentPage = 4;
     $scope.prevPage = '/step-3';
 
-    this.save = () => {
-      //
+    this.select = (text) => {
+      this.error = null;
+      active(event.target);
+
+      if(text === 'cat'){
+        $scope.avatar = event.target.src;
+        $scope.page4 = true;
+        $scope.nextPage = '/result';
+      } else {
+        $scope.nextPage = '';
+        $scope.page4 = false;
+        this.error = 'Вы выбрали собачку. А надо котика.';
+      }
+    };
+
+    function active (element) {
+      let images = element.parentNode.childNodes;
+      for (let i = 0; i < images.length; i++) {
+        if(i%2 !== 0){
+          images[i].style.border = 'none';
+        } 
+      }
+      element.style.border = '3px solid #ff9800';
     }
+    
   }
 }
 
--- source/step-4/index.js
+++ source/step-4/index.js
@@ -3,8 +3,9 @@ import uirouter from 'angular-ui-router';
 
 import routing from './routes';
 import FourStepController from './controller';
+import val from '../directive.js';
 
-export default angular.module('app.fourStep', [uirouter])
+export default angular.module('app.fourStep', [uirouter, val])
   .config(routing)
   .controller('FourStepController', FourStepController)
   .name;
