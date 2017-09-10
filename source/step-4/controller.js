317a536cf313ec8143ce54e464a03764c0a80353
yarik-vv <yarik.vashchuk@gmail.com>
2017-09-10 19:47:34 +0300

    added styles, images for four step, new template structure, finish button

--- source/step-4/controller.js
+++ source/step-4/controller.js
@@ -1,11 +1,25 @@
+const nextBtn = document.getElementById('next');
+const resultBtn = document.getElementById('result');
+// nextBtn.style.display = 'none';
+// resultBtn.style.display = 'flex';
+
+import { validate, reset } from '../validation';
+import { initPagination, setState, buttonState } from '../pagination';
+
 class FourStepController {
   constructor($scope) {
     $scope.title = 'Четвертый шаг анкеты';
     $scope.stepName = '4. Выберите любимого котика';
-    $scope.nextPage = '';
+    $scope.nextPage = '/result';
     $scope.page4 = false;
-    $scope.currentPage = 4;
+    //$scope.currentPage = 4;
     $scope.prevPage = '/step-3';
+
+    initPagination($scope.page1, $scope.page2, $scope.page3, $scope.page4, 4);
+    buttonState('none', 'flex');
+    ///setState($scope.page4);
+    nextBtn.style.display = 'none';
+    resultBtn.style.display = 'flex';
 
     this.select = (text) => {
       this.error = null;
@@ -14,10 +28,12 @@ class FourStepController {
       if(text === 'cat'){
         $scope.avatar = event.target.src;
         $scope.page4 = true;
-        $scope.nextPage = '/result';
+        //$scope.nextPage = '/result';
+        setState(true); 
       } else {
-        $scope.nextPage = '';
+        //$scope.nextPage = null;
         $scope.page4 = false;
+        setState(false); 
         this.error = 'Вы выбрали собачку. А надо котика.';
       }
     };
