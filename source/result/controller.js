9c8ebc5a07981bec17c4bf66ccf51862714b51aa
yarik-vv <yarik.vashchuk@gmail.com>
2017-09-11 21:37:16 +0300

    configured controller for result page

--- source/result/controller.js
+++ source/result/controller.js
@@ -1,6 +1,15 @@
+const navigation = document.getElementsByTagName('nav')[0];
+const pagination = document.getElementById('pagination');
+const ava = document.getElementById('avatar');
+
 class ResultController {
   constructor($scope) {
     $scope.title = 'Ваш результат';
+    $scope.stepName = '';
+    navigation.style.display = 'none';
+    pagination.style.display = 'none';
+    avatar.style.background = 'url(' + $scope.user.avatar + ') center';
+    avatar.style.backgroundSize = 'cover';
 
     this.reset = () => {
       //
