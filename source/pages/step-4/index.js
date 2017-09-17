02984271d3fa77d327d43004879cbfa1140719ab
yarik-vv <yarik.vashchuk@gmail.com>
2017-09-17 17:46:40 +0300

    gh-pages build

--- source/pages/step-4/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-import uirouter from 'angular-ui-router';
-import routing from './routes';
-import StepFourController from './controller';
-
-export default angular.module('app.stepFour', [uirouter])
-  .config(routing)
-  .controller('StepFourController', StepFourController)
-  .name;
